41d3f9ec3e61489c04f4c7a418ee28734e5511e7
ARMAN\user <armanvirabyan15@gmail.com>
2025-09-01 20:36:05 +0400

    feat: draw triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,20 @@
+#include <GL/gl.h>
+
 #include "graphics/Window.h"
 
 using namespace Engine::Graphics;
 
 int main() {
     Window window("Game", 800, 600);
+    glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
     while (!window.closed())
     {
+        window.clear();
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f( 0.5f, -0.5f);
+        glVertex2f( 0.0f,  0.5f);
+        glEnd();
         window.update();
     }
     return 0;
